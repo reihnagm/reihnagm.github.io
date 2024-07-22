833f41659ebe9cb8ad367b659b8a7699c8b9ac21
Reihan Agam <reihanagam7@gmail.com>
2024-07-23 00:04:59 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Times New Roman"', 'serif'],
+            },
+        },
+    },
+    plugins: [],
+};
+  
